03a2ccb28ebae2d181df12ea11af11af67b1618a
afeiship <1290657123@qq.com>
2024-05-14 09:15:24 +0800

    refactor: big change use grid

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,29 +1,10 @@
 import noop from '@jswork/noop';
 import cx from 'classnames';
-import React, { ReactNode, createRef, Component, HTMLAttributes } from 'react';
+import React, { ReactNode, Component, HTMLAttributes } from 'react';
 
 // grid way: https://dev.to/alexandprivate/the-ultimate-collapsible-component-with-height-auto-detection-25pi
 
 const CLASS_NAME = 'react-collapse';
-const preloadImage = (src: string) =>
-  new Promise((resolve, reject) => {
-    const image = new Image();
-    image.onload = resolve;
-    image.onerror = reject;
-    image.src = src;
-  });
-
-const getElementRect = async (el: HTMLElement): Promise<DOMRect> => {
-  const clone = el.cloneNode(true) as HTMLElement;
-  const imgs = Array.from(clone.getElementsByTagName('img'));
-  await Promise.all(imgs.map((img) => preloadImage(img.src)));
-  clone.style.cssText = 'position:fixed;top:0;left:0;max-height:unset;';
-  document.body.append(clone);
-  const rect = clone.getBoundingClientRect();
-  clone.remove();
-  return rect;
-};
-
 
 export type ReactCollapseProps = {
   /**
@@ -42,11 +23,6 @@ export type ReactCollapseProps = {
    */
   onChange?: (collapsed: boolean) => void;
   /**
-   * The max height of content.
-   * @default 0
-   */
-  maxHeight?: number;
-  /**
    * The summary element.
    */
   summary?: ReactNode;
@@ -62,15 +38,6 @@ export default class ReactCollapse extends Component<ReactCollapseProps, ReactCo
   static defaultProps = {
     collapsed: false,
     onChange: noop,
-    maxHeight: 0,
-  };
-
-  private elementRef = createRef<HTMLDivElement>();
-  private updateElementMaxHeight = (maxHeight: number) => {
-    const el = this.elementRef.current;
-    if (el) {
-      el.style.maxHeight = `${maxHeight}px`;
-    }
   };
 
   state = {
@@ -87,40 +54,22 @@ export default class ReactCollapse extends Component<ReactCollapseProps, ReactCo
     );
   }
 
-  async getElementRect() {
-    const { maxHeight } = this.props;
-    if (maxHeight) return { height: maxHeight };
-    const el = this.elementRef.current;
-    if (el) return await getElementRect(el);
-    return null;
-  }
-
-  async componentDidMount() {
-    const { elementRef } = this;
-    const el = elementRef.current;
-    if (el) {
-      const rect = await this.getElementRect();
-      if (!rect) console.warn('ReactCollapse: elementRect is null.');
-      this.updateElementMaxHeight(rect?.height || 0);
-    }
-  }
-
   shouldComponentUpdate(nextProps: Readonly<ReactCollapseProps>): boolean {
-    const { maxHeight, collapsed } = nextProps;
-    if (collapsed !== this.props.collapsed) this.setState({ collapsed });
-    if (maxHeight !== this.props.maxHeight) this.updateElementMaxHeight(maxHeight!);
+    const { collapsed } = nextProps;
+    if (collapsed !== this.state.collapsed) this.setState({ collapsed });
     return true;
   }
 
   handleSummaryClick = () => {
     const { onChange } = this.props;
-    this.setState({ collapsed: !this.state.collapsed }, () => {
-      onChange?.(this.state.collapsed!);
+    const { collapsed } = this.state;
+    this.setState({ collapsed: !collapsed }, () => {
+      onChange?.(!collapsed);
     });
   };
 
   render() {
-    const { className, children, summary, collapsed, maxHeight, onChange, ...rest } = this.props;
+    const { className, children, summary, collapsed, onChange, ...rest } = this.props;
     return (
       <section
         data-component={CLASS_NAME}
@@ -129,7 +78,11 @@ export default class ReactCollapse extends Component<ReactCollapseProps, ReactCo
         {...rest}
       >
         {this.summaryView}
-        <article ref={this.elementRef} className={cx(`${CLASS_NAME}__body`)}>{children}</article>
+        <article className={cx(`${CLASS_NAME}__body`)}>
+          <div className={`${CLASS_NAME}__content`}>
+            {children}
+          </div>
+        </article>
       </section>
     );
   }
